2b520b747018c21adfeae85b156dab4d413ab7dc
Darina Pavlova <pavlovdarina@gmail.com>
2023-11-14 17:06:56 +0200

    Refactoring

--- Java-based testing project for Jira.com/src/main/java/com/telerikacademy/api/tests/Constants.java
+++ Java-based testing project for Jira.com/src/main/java/com/telerikacademy/api/tests/Constants.java
@@ -5,19 +5,23 @@ public class Constants {
 
     public static final String EMAIL = "";
     public static final String JIRA_API_TOKEN = "";
-    public static final String BASE_URL = "https://xray-demo-project-a50.atlassian.net";
+    public static final String BASE_URL = "";
     public static final String PROJECT_KEY = "TR";
-    public static final String STORY_SUMMARY = "Create JIRA story homework";
-    public static final String BUG_SUMMARY = "Staging forum is not available";
+    public static final String STORY_SUMMARY = "German User Wants to View the Page in German";
+    public static final String BUG_SUMMARY = "Website does not switch to German when selected";
     public static final String STORY_NAME = "Story";
     public static final String BUG_NAME = "Bug";
     public static final String BUG_PRIORITY = "Highest";
-    public static final String BUG_DESCRIPTION = "*Steps to reproduce:*\\n"
-            + "1. Navigate to the main page - https://stage-forum.telerikacademy.com\\n"
-            + "*Expected result:* \\n The staging forum is loaded successfully \\n"
-            + "*Actual result:* Staging forum is not available and return error: \\\"We can't connect to the server at \\\"stage-forum.telerikacademy.com.\\\" \\n"
-            + "*Severity:* Blocking \\n"
-            + "*Prerequisites:* N/A \\n !https://upload.wikimedia.org/wikipedia/commons/f/f8/Science_2.0_model.png!";
+    public static final String BUG_DESCRIPTION = "As a user, I want the website to switch to German when I select German from the language options on phptravels.net/en, in order to navigate the site and understand the content" +
+            "\\nPreconditions:\\nGoogle Chrome Version 114.0.5735.199 (Official Build) (64-bit)" +
+            "\\nSteps to reproduce:" +
+            "\\n1. Navigate to phptravels.net/en in a web browser." +
+            "\\n2. Click on the Language Bar located at the top right corner of the homepage." +
+            "\\n3. From the dropdown menu, select 'German'" +
+            "\\nExpected Result:" +
+            "\\n1. The site content should immediately switch to display in German." +
+            "\\nActual Result:" +
+            "\\n1. The site content remains in English.";
     public static String DEFAULT_ACCOUNT_ID = "";
     public static String ACCOUNT_ID = "";
     public static String STORY_KEY;
